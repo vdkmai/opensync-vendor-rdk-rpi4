1f0bbdc8a1ae50741eba9940e3bf88a03d3c0f2c
Lars Kristan <lkristan@plume.com>
2019-11-25 16:00:01 +0100

    Release 1.4.0.0

--- src/lib/osync_hal/override.mk
+++ src/lib/osync_hal/override.mk
@@ -22,35 +22,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-ifeq ($(TARGET),RDKB)
-
-# TODO: Set correct vendor name
-VENDOR = template
-
-BACKHAUL_SSID = "we.piranha"
-
-CONTROLLER_PROTO = ssl
-CONTROLLER_PORT = 443
-CONTROLLER_HOST = "wildfire.plume.tech"
-
-VERSION_NO_BUILDNUM = 1
-VERSION_NO_SHA1 = 1
-VERSION_NO_PROFILE = 1
-
-# TODO: Set correct machine (it should equal to MACHINE variable from your Yocto build)
-ifeq ($(RDK_MACHINE),template_machine)
-
-# TODO: Set correct OEM and model names
-RDK_OEM = template_oem
-RDK_MODEL = template_model
-
-KCONFIG_TARGET ?= platform/rdk/kconfig/RDK
-
-# TODO: Specify additional CFLAGS if needed
-#RDK_CFLAGS  +=
-
-else
-$(error Unsupported RDK_MACHINE ($(RDK_MACHINE)).)
-endif
-
-endif
+UNIT_SRC_TOP := $(OVERRIDE_DIR)/src/osync_hal.c
+UNIT_CFLAGS  += -I$(OVERRIDE_DIR)/inc
+UNIT_DEPS    += src/lib/schema
+UNIT_EXPORT_CFLAGS := ${UNIT_CFLAGS}
